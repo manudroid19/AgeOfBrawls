6e6c466045a8ad225ac4aab23326023254795c68
prada <prada@prada-arch.localdomain>
2017-10-16 13:08:43 +0200

    empezando

--- src/ageofbrawls/AgeOfBrawls.java
+++ src/ageofbrawls/AgeOfBrawls.java
@@ -15,7 +15,8 @@ public class AgeOfBrawls {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Mapa mapa = new Mapa();
+        mapa.imprimir();
     }
     
 }
