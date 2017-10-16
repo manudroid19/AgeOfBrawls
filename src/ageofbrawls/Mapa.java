7afa82c99ed542417102c1721ffe388f2c45d9b5
prada <prada@prada-arch.localdomain>
2017-10-16 12:58:06 +0200

    mapa, construccion

--- src/ageofbrawls/Mapa.java
+++ src/ageofbrawls/Mapa.java
@@ -19,7 +19,7 @@ public class Mapa {
     public Mapa(int filas, int columnas){
         mapa = new ArrayList();
         for(int i=0;i<filas;i++){
-            ArrayList<Celda> b = new ArrayList();
+            ArrayList<Celda> b = new ArrayList(columnas);
             mapa.add(b);
         }
         this.filas = filas;
@@ -36,8 +36,26 @@ public class Mapa {
     public Celda getCelda(int i, int j){
         return mapa.get(i).get(j);
     }
-    boolean esCeldaVacia (int i, int j){
+    public boolean esCeldaVacia (int i, int j){
         return (mapa.get(i).get(j) == null); //expresion logica= true si celda==null
     }
-    
+    public void imprimir(){
+        for(int i=0;i<columnas+2;i++)
+            System.out.print("_");
+        System.out.println();
+        
+        for(int i=0;i<filas;i++){
+            System.out.println("|");
+            for(int j=0;j<columnas;j++){
+                System.out.println(mapa.get(i).get(j));
+            }
+            System.out.println("|");
+
+        }
+        
+        
+        
+        for(int i=0;i<columnas+2;i++)
+            System.out.print("_");
+    }
 }
