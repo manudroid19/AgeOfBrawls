065364fc529bc2f0178de46081535e4a06d8d91a
Santiago <Santiago@LAPTOP-UFFR3QM9>
2017-10-16 12:23:48 +0200

    Creado clase Celda

---
+++ src/ageofbrawls/Celda.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ageofbrawls;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Celda {
+    private int cantidadRec;
+    private Edificio edificio;
+
+    public int getCantidadRec() {
+        
+        return cantidadRec;
+    }
+    public void setCantidadRec(int valor){
+        if(valor>(-1)){
+            cantidadRec=valor;
+        }
+        else
+            throw Exception;
+    }
+    
+    
+    
+}
