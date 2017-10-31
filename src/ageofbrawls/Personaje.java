7cb5705594544843aa439fee5e4e8a228ae618ad
Santiago <Santiago@LAPTOP-UFFR3QM9>
2017-10-31 12:04:36 +0100

    Excepciones setters ContRec

--- src/ageofbrawls/Personaje.java
+++ src/ageofbrawls/Personaje.java
@@ -17,10 +17,11 @@ public class Personaje {
     private int armadura;
     private int ataque;
     private int capRec;
+    private int cantRec;
     private boolean estaMuerto;
     
     public Personaje(int tipo){
-        if(tipo>=1&&tipo<=2)
+        if(tipo>=1 && tipo<=2)
             this.tipo=tipo;
     }
     public int getTipo(){
@@ -41,6 +42,60 @@ public class Personaje {
 
     public int getCapRec() {
         return capRec;
+    }
+    public void setCapRec(int valor){
+        if(valor>0){
+            capRec=valor;
+        }
+        else
+            System.out.println("Capacidad introducida errónea");
+    }
+    public void CrearSoldado(){
+        tipo=2;
+        salud=100;
+        armadura=200;
+        ataque=30;
+        estaMuerto=false;
+    }
+    public void CrearPaisano(){
+        tipo=1;
+        salud=50;
+        armadura=100;
+        ataque=0;
+        capRec=40;
+        estaMuerto=false;
+    }
+    public void describirPersonaje(){
+        
+    }
+  
+    public void recolectar(Personaje personaje){
+        if(personaje.getTipo()==1){
+ 
+            
+            
+        }
+        else
+            System.out.println("Un soldado no puede recolectar");
+    }
+    public void consEdif(Personaje personaje){
+         if(personaje.getTipo()==1){
+            
+            
+            
+        }
+        else
+            System.out.println("Un soldado no puede construir edificios");
+    } 
+    
+    public void almacenar(Personaje personaje){
+        if(personaje.getTipo()==1){
+            
+            
+            
+        }
+        else
+            System.out.println("Un soldado no puede almacenar recursos ");
     }
     
 }
